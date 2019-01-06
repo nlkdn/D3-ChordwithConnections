04c4407f463b0f8da46090da804fd44deece24d0
Neelakandan K <nlkdn.engineer@gmail.com>
2019-01-06 22:54:56 +0530

    imporved with d3.arc and pie

--- main.js
+++ main.js
@@ -8,63 +8,15 @@ const draw = () => {
     //Declare svg
     const svg = d3.select('body')
         .append('svg')
-        .attr('viewBox', `0 0 ${width+margin} ${height+margin}`)
-        // .attr('width', width+margin)
-        // .attr('height',height+margin) 
-
-    //Create nodes function
-    // const createNodes = (numNodes, radius) => {
-    //     let nodes = [], 
-    //     angle,
-    //     x,
-    //     y,
-    //     i;
-    // for (i=0; i<numNodes; i++) {
-    //     angle = (i / (numNodes/2)) * Math.PI; // Calculate the angle at which the element will be placed.
-    //                                         // For a semicircle, we would use (i / numNodes) * Math.PI.
-    //     x = (radius * Math.cos(angle))+(width/2);// Calculate the x position of the element.
-    //     y = (radius * Math.sin(angle))+(height/1.6); // Calculate the y position of the element.
-    //     nodes.push({'id': i, 'x': x, 'y': y, 'value':i, 'angle':angle});
-    // }
-    // return nodes;
-    // // console.log(nodes)
-    // }
+        .attr('viewBox', `0 0 ${width+margin} ${height+margin}`);
 
     //Create nodes using d3.pie
     const createNodes = (numNodes, radius) => {
-        let nodes = [], 
-        angle,
-        x,
-        y,
-        i;
-
-        const arc = d3.arc()
-                .innerRadius(radius)
-                .outerRadius(radius)
-                // .startAngle(nodes[8].angle+1.5)
-                // .endAngle(nodes[27].angle+1.5);
-                .padAngle(0.05)
-                .padRadius(25)
-                .cornerRadius(40);
-        const pie =d3.pie()
-                    .value (d => d.value);
+        let nodes = [];
         for (i=0; i<numNodes; i++) {
-            nodes.push({'id': i, 'value':1, 'angle':angle});
+            nodes.push({'id': i, 'value':1});
         }
-        //return nodes;
-        // console.log(nodes)
-
-        const path = d3.select('svg')
-                    .append('g')
-                    .selectAll('path')
-                    .data(pie(nodes))
-                    .enter()
-                    .append('path')
-                    .attr('d', arc)
-                    //.attr('fill','green')
-                    //.attr('stroke', 'white')
-                    .attr('transform', `translate(${width/2},${height/1.6})`);
-
+        return nodes;
     }
 
     const innerNodes = createNodes(26, 90);
@@ -75,72 +27,100 @@ const draw = () => {
 
     allNodes = allNodes.concat(outerNodes);
 
-    // console.log(innerNodes[0]);
+    const createCircle = (nodes,radius) => {
+        const arcs = d3.arc()
+                    .innerRadius(radius)
+                    .outerRadius(radius+20)
+                    .padAngle(0.2)
+                    .padRadius(25)
+                    .cornerRadius(40);
+        const pie = d3.pie()
+                    .value(d => d.value);
 
-    // //create circles
-    // const circles = d3.select('svg')
-    //                     .append('g')
-    //                     .selectAll('circle')
-    //                     .data(allNodes)
-    //                     .enter()
-    //                     .append('circle')
-    //                     .attr('r', 8)
-    //                     .attr('cx', d => d.x)
-    //                     .attr('cy', d => d.y);
+        const path = d3.select('svg')
+                    .append('g')
+                    .attr('transform', `translate(${width/2},${height/1.6})`)
+                    .selectAll('path')
+                    .data(pie(nodes))
+                    .enter()
+                    .append('path')
+                    .attr('d', arcs)
+                    .attr('class','arcs');
+                    //.attr('fill','green')
+                    //.attr('stroke', 'white')
 
-    const nodes = allNodes.slice(),
-        nodeById = d3.map([nodes[0],nodes[8],nodes[27],nodes[59]], d => d.id),
-        links = [{"source": nodes[0].id, "target": nodes[8].id, "value": 1},
-                // {"source": nodes[8].id, "target": nodes[27].id, "value": 1},
-                 {"source": nodes[27].id, "target": nodes[59].id, "value": 1}],
-        bilinks = [];
-      links.forEach(function(link) {
-        let s = link.source = nodeById.get(link.source),
-            t = link.target = nodeById.get(link.target),
-            i = {x: link.target.x, y: link.source.y}; // intermediate node
-        nodes.push(i);
-        links.push({source: s, target: i}, {source: i, target: t});
-        bilinks.push([s, i, t]);
-      });
+        const dot =  d3.symbol();
+        const dots = d3.select('svg')
+                    .append('g')
+                    .attr('transform', `translate(${width/2},${height/1.6})`)
+                    .selectAll('path')
+                    .data(pie(nodes))
+                    .enter()
+                    .append('path')
+                    .attr("transform", d => `translate(${arcs.centroid(d)})`)
+                    .attr('d',dot)
+                    .attr('color','green')
+                    .attr('fill','green')
+                    .attr('class','dots');
 
-    // create links by force simulation
-    const simulation = d3.forceSimulation()
-                        .force("link", d3.forceLink(links))
-                        .force("charge", d3.forceManyBody())
-                        .force("center", d3.forceCenter(width / 2, height / 1.6))
-                        .on("tick", ticked);
+        const centroid = pie(nodes).map(function(d, i) {
+            d.innerRadius = radius;
+            d.outerRadius = radius+20;
+            d.data.x = arcs.centroid(d)[0]+width/2;
+            d.data.y = arcs.centroid(d)[1]+height/1.6;
+            d.data.endAngle = d.endAngle; 
+            d.data.startAngle = d.startAngle; 
+            return d.data;
+          })
 
-    const group = svg.append('g');
+        return centroid;
+        console.log(centroid)
+    }
 
-    //tick function
-    function ticked() {
-        const link = group
+    const innerCircle = createCircle(innerNodes, 90);
+    const midCircle = createCircle(midNodes, 180);
+    const outerCircle = createCircle(outerNodes, 260);
+
+    const innerConnection = (source, target) => {
+        const links = [{"source": source, "target": target, "value": 1}]
+            // {"source": centroid[8].id, "target": centroid[27].id, "value": 1},
+                //{"source": centroid[27], "target": centroid[59], "value": 1}],
+            bilinks = [];
+        links.forEach(function(link) {
+            let s = link.source //= nodeById.get(link.source),
+                t = link.target //= nodeById.get(link.target),
+                i = {x: link.target.y, y: link.source.x}; // intermediate node
+            links.push({source: s, target: i}, {source: i, target: t});
+            bilinks.push([s, i, t]);
+        });
+        // create links by force simulation
+        const simulation = d3.forceSimulation()
+                    .force("link", d3.forceLink(links))
+                    .force("charge", d3.forceManyBody())
+                    .force("center", d3.forceCenter(width / 2, height / 1.6))
+                    .on("tick", ticked);
+
+        const group = svg.append('g');
+
+        //tick function
+        function ticked() {
+            const link = group
                 .selectAll('path')
                 .data(bilinks)
                 .enter().append("path")
                 //.attr("class", "link")
                 .attr("d", positionLink);
-    }
+        }
 
-    //curved path
-    const positionLink = d  => `M${d[0].x},${d[0].y} Q${d[1].x},${d[1].y} ${d[2].x},${d[2].y}`;
+        //curved path
+        const positionLink = d  => `M${d[0].x},${d[0].y} Q${d[1].x},${d[1].y} ${d[2].x},${d[2].y}`;
 
-    // //pie
-    // pie = d3.pie()
-    //         // .sort(null)
-    //         // .value(d => d.value)
+    }        
+        
+    innerConnection(innerCircle[0], innerCircle[8]);
 
-    const radius = 110;
-    const arc = d3.arc()
-                .innerRadius(radius)
-                .outerRadius(radius)
-                .startAngle(nodes[8].angle+1.5)
-                .endAngle(nodes[27].angle+1.5);
-                //.padAngle(0.05)
-                //.padRadius(25)
-                //.cornerRadius(40);
-
-    const path = d3.select('svg')
+    /*
+    const paths = d3.select('svg')
                 .append('g')
                 .selectAll('path')
                 .data(d3.pie()(1))
@@ -152,17 +132,17 @@ const draw = () => {
                 .attr('transform', `translate(${width/2},${height/1.6})`);
 
     const points = [
-            //nodes[8],nodes[27]
+            //centroid[8],centroid[27]
         ];
         let radialObj = {}
         let r = 90;
-        let a = nodes[8].angle;
+        let a = centroid[8].angle;
         radialObj.a=a;
         radialObj.r=r;
         points.push(radialObj);
         r=radius;
-        for(i=nodes[27].angle;i< nodes[8].angle; i++) {
-            if(i==nodes[27].angle) {
+        for(i=centroid[27].angle;i< centroid[8].angle; i++) {
+            if(i==centroid[27].angle) {
                 r = 180;
             } 
             let Obj ={}
@@ -170,8 +150,6 @@ const draw = () => {
             Obj.r=r;
             points.push(Obj);
         }
-    console.log(points)
-    console.log(Math.sin(nodes[27].angle));
 
 
 
@@ -198,6 +176,7 @@ const draw = () => {
     .append('path')
     .attr('d',line(points))
     .attr('transform', `translate(${width/2},${height/1.6})`);
+    */
 }
 
 draw();
